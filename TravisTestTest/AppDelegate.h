195ff2affcd4d9417f96eff558dc1fb70c99a101
Soj <s.sojik@gmail.com>
2013-12-13 14:03:50 +0700

    Initial Commit

---
+++ TravisTestTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TravisTestTest
+//
+//  Created by Сергей on 13.12.13.
+//  Copyright (c) 2013 Sergey Mingalev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravisTestTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TravisTestTest
+//
+//  Created by Сергей on 13.12.13.
+//  Copyright (c) 2013 Sergey Mingalev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
